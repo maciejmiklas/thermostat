415bf3005ff87ce979a93e7f415eaf91d15a1e4a
Maciej Miklas <mac.miklas@gmail.com>
2016-07-15 12:14:45 +0200

    imports

---
+++ Thermostat.cpp
@@ -0,0 +1,9 @@
+#include "Thermostat.h"
+
+void setup() {
+	util_setup();
+}
+
+void loop() {
+	util_cycle();
+}
---
+++ Thermostat.h
@@ -0,0 +1,10 @@
+#ifndef Thermostat_H_
+#define Thermostat_H_
+
+#include "Arduino.h"
+#include "Timer.h"
+
+void loop();
+void setup();
+
+#endif /* Thermostat_H_ */
